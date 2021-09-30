30b855f85b0e69524436750d2cb373c2c2c6f1ff
Divyajg <guttuladivyajyothi@gmail.com>
2021-09-30 12:53:05 +0200

    Now the code checks for valid name of the city.

--- javascript/javascript3/week1/weatherApi.js
+++ javascript/javascript3/week1/weatherApi.js
@@ -1,42 +1,47 @@
 //Weather API------------------------------------------------------------------
 //get weather for your choice of city------------------------------------------
 
-const url = c20bdbd9cdf94e9fb3889776572cba5a;
+const apiKey = "c20bdbd9cdf94e9fb3889776572cba5a";
 
 function getWeatherHere() {
     const city = document.getElementById('cityInput').value;
     if (city) {
-        const weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + url;
+        const weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey;
+        console.log(weatherUrl);
 
-        document.getElementById('city').innerHTML = "City:  " + city;
+
         fetch(weatherUrl)
             .then(response => response.json())
             .then(weather => {
-                document.getElementById('temp').innerHTML = "Temperatur(F):  " + weather.main.temp;
-                document.getElementById('icon').src = "http://openweathermap.org/img/w/" + weather.weather[0].icon + ".png";
-                document.getElementById('wind').innerHTML = "Wind Speed(Kmps):  " + weather.wind.speed;
-                document.getElementById('cloud').innerHTML = "Clouds:  " + weather.clouds.all;
-                document.getElementById('sunrise').innerHTML = "Sunrise:  " + weather.sys.sunrise;
-                document.getElementById('sunset').innerHTML = "Sunset:  " + weather.sys.sunset;
-                //saving data to localstorage:---------------------------------------------
-                localStorage.setItem('city', city);
-                let retriveData = localStorage.getItem('city');
-                console.log('retrived data:  ', retriveData);
-                //showing map-------------------------------
-                let lat = weather.coord.lat;
-                let lng = weather.coord.lon;
-                let map;
+                if (weather.message === "city not found") {
+                    document.getElementById('note').innerHTML = "Please provide the valid city name!"
+                } else {
+                    document.getElementById('city').innerHTML = "City:  " + city;
+                    document.getElementById('temp').innerHTML = "Temperatur(F):  " + weather.main.temp;
+                    document.getElementById('icon').src = "http://openweathermap.org/img/w/" + weather.weather[0].icon + ".png";
+                    document.getElementById('wind').innerHTML = "Wind Speed(Kmps):  " + weather.wind.speed;
+                    document.getElementById('cloud').innerHTML = "Clouds:  " + weather.clouds.all;
+                    document.getElementById('sunrise').innerHTML = "Sunrise:  " + weather.sys.sunrise;
+                    document.getElementById('sunset').innerHTML = "Sunset:  " + weather.sys.sunset;
+                    //saving data to localstorage:---------------------------------------------
+                    localStorage.setItem('city', city);
+                    let retriveData = localStorage.getItem('city');
+                    console.log('retrived data:  ', retriveData);
+                    //showing map-------------------------------
+                    let lat = weather.coord.lat;
+                    let lng = weather.coord.lon;
+                    let map;
 
-                function initMap(lat, lng) {
-                    const coords = new google.maps.LatLng(lat, lng);
-                    map = new google.maps.Map(document.getElementById("map"), {
-                        zoom: 4,
-                        center: coords,
-                    });
+                    function initMap(lat, lng) {
+                        const coords = new google.maps.LatLng(lat, lng);
+                        map = new google.maps.Map(document.getElementById("map"), {
+                            zoom: 4,
+                            center: coords,
+                        });
+                    }
+                    initMap(lat, lng);
+                    document.getElementById('map').innerHTML = `<div><iframe src="https://maps.google.com/maps?q=${weather.name}&t=&z=11&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe></div>`;
                 }
-                initMap(lat, lng);
-                document.getElementById('map').innerHTML = `<div><iframe src="https://maps.google.com/maps?q=${weather.name}&t=&z=11&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe></div>`;
-
             })
             .catch((error) => console.log(error));
     } else {
@@ -59,10 +64,10 @@ function success(pos) {
     let crd = pos.coords;
     const cLocation = document.getElementById('displayCurrentLoc');
     cLocation.innerHTML = "Latitude= " + crd.latitude + "  Longitude= " + crd.longitude;
-    const myurl = 'https://api.openweathermap.org/data/2.5/weather?lat=' + crd.latitude + '&lon=' + crd.longitude + '&appid=' + url;
+    const myurl = 'https://api.openweathermap.org/data/2.5/weather?lat=' + crd.latitude + '&lon=' + crd.longitude + '&appid=' + apiKey;
     console.log(myurl);
 
-    fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + crd.latitude + '&lon=' + crd.longitude + '&appid=' + url)
+    fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + crd.latitude + '&lon=' + crd.longitude + '&appid=' + apiKey)
         .then(response => response.json())
         .then(data => {
             document.getElementById('currentTemp').innerHTML = "Temperatur(F):  " + data.main.temp;
